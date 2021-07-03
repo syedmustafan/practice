b76ba91fcbf40031794e1c6ba5d781b64c30e693
Syed Mustafan <mustafanadeem627@gmail.com>
2021-07-04 01:08:31 +0500

    Question 4

---
+++ q4.py
@@ -0,0 +1,22 @@
+lst = [3,2,1,4]
+
+def candyStore(lst, N, K):
+    diff = N - K
+    if diff == 1:
+        minimum = min(lst)
+        maximum = max(lst)
+    else:
+        minimum = 0
+        maximum = 0
+        sorted_list = sorted(lst)
+        min_lst = sorted_list[:diff]
+        max_lst = sorted_list[-diff:]
+        
+        for i in range(len(min_lst)):
+            minimum = min_lst[i] + minimum
+        for j in range(len(min_lst)):
+            maximum = max_lst[j] + maximum    
+            
+            
+    print(minimum, maximum)
+candyStore(lst, 4, 2)
